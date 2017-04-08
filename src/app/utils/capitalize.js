1123af9383f23241afe001bfa3bc7ff88f525ce8
Sylphony <symphonictwist@gmail.com>
2017-04-08 13:05:10 -0700

    Keep it simple

--- src/app/utils/capitalize.js
+++ src/app/utils/capitalize.js
@@ -1,7 +1,10 @@
+/**
+ * Capitalize a string.
+ * @param {str} str: The string.
+ * @return {str}: A string that is uppercased if input was a string.
+ */
 export default function captialize(str) {
     if (typeof str === "string") {
-        return str.replace(/^[a-z]/gi, (match, offset, string) => {
-            return match.toUpperCase();
-        });
+        return str.slice(0, 1).toUpperCase() + str.slice(1);
     }
 }
